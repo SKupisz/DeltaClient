b695b3f12f321cc51b3bbaa901ef9d5c6d5978df
SKupisz <szym-ku@wp.pl>
2019-10-01 08:40:27 +0200

    Cyphering and decoding works completly

--- index.php
+++ index.php
@@ -2,19 +2,61 @@
     if(isset($_POST["access-key"]) && isset($_POST["cypher-content"])){
         $content = $_POST["cypher-content"];
         $content = rawurlencode($content);
-        $content = htmlentities($content);
-        if(isset($_POST["encryptOrdecrypt"])){
-            $askQuery = "localhost/delta/deltaApiCenter/api.php?method=mate&a={$_POST['access-key']}&c={$content}";
+        //$content = htmlentities($content);
+        if(strlen($content) > 100){
+            $toComplete = "";
+            if(!isset($_POST["encryptOrdecrypt"])){
+                $alpha = explode("k",$content);
+                for($i = 0 ; $i < count($alpha); $i++){
+                    $nowPart = $alpha[$i];
+                    $askQuery = "localhost/delta/deltaApiCenter/api.php?method=friend&a={$_POST['access-key']}&c={$nowPart}";
+                    $toDo = curl_init($askQuery);
+                    curl_setopt($toDo,CURLOPT_RETURNTRANSFER,true);
+                    $result = curl_exec($toDo);
+                    $response = json_decode($result);
+                    $toComplete.=$response;
+                }
+            }
+            else{
+                for($i = 0 ; $i < strlen($content); $i+=100)
+                {
+                    $flag = 0;
+                  if($i + 100  < strlen($content))
+                  {
+                    $forNowText = substr($content,$i,100);
+                  }
+                  else {
+                    $forNowText = substr($content,$i,(strlen($content)-$i));
+                    $flag = 1;
+                  }
+                    $askQuery = "localhost/delta/deltaApiCenter/api.php?method=mate&a={$_POST['access-key']}&c={$forNowText}";
+                  $toDo = curl_init($askQuery);
+                  curl_setopt($toDo,CURLOPT_RETURNTRANSFER,true);
+                  $result = curl_exec($toDo);
+                  $response = json_decode($result);
+                  if($_POST["access-key"] && $flag == 0){
+                      $toComplete.=$response;
+                      $toComplete.="k";
+                  }
+                  //echo $response."<br>Completed: ".$toComplete."<br>";
+                }
+            }
+            $iscypher = 1;
         }
         else{
-            $askQuery = "localhost/delta/deltaApiCenter/api.php?method=friend&a={$_POST['access-key']}&c={$content}";
+            if(isset($_POST["encryptOrdecrypt"])){
+                $askQuery = "localhost/delta/deltaApiCenter/api.php?method=mate&a={$_POST['access-key']}&c={$content}";
+            }
+            else{
+                $askQuery = "localhost/delta/deltaApiCenter/api.php?method=friend&a={$_POST['access-key']}&c={$content}";
+            }
+            $toDo = curl_init($askQuery);
+            curl_setopt($toDo,CURLOPT_RETURNTRANSFER,true);
+            $result = curl_exec($toDo);
+            $toComplete = json_decode($result);
+            $iscypher = 1;
         }
-        
-        $toDo = curl_init($askQuery);
-        curl_setopt($toDo,CURLOPT_RETURNTRANSFER,true);
-        $result = curl_exec($toDo);
-        $response = json_decode($result);
-        $iscypher = 1;
+
     }
     else{
         $iscypher = 0;
@@ -34,7 +76,7 @@
     <?php if($iscypher == 1){
         ?><section class = "returnSection">
             <div class = "returnHeader">Your message is: </div>
-            <main class = "message-container"><?php echo $response; ?></main>
+            <main class = "message-container"><?php echo $toComplete; ?></main>
             <nav class = "buttons-nav">
                 <button class = "button clip-button">Copy to clipboard</button>
                 <button type = "submit" class = "resetBtn">Go again</button>
